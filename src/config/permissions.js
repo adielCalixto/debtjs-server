fce3e4c4ce4f3b67aa7cda3c51c0f99730a7328f
adielCalixto <adielsemr@protonmail.com>
2021-12-10 15:17:03 -0300

    updated permissions

--- src/config/permissions.js
+++ src/config/permissions.js
@@ -1,16 +1,24 @@
-module.exports = {
+const permissions = {
     "usuario": {
         "canCreate": [],
         "expires": "10d",
+        "canUpdate": [ "usuario" ],
+        "canDelete": [ "usuario" ],
     },
 
     "admin": {
         "canCreate": [ "usuario", "gestor" ],
         "expires": "30m",
+        "canUpdate": [ "admin", "usuario", "gestor" ],
+        "canDelete": [ "usuario", "gestor" ],
     },
 
     "gestor": {
         "canCreate": [ "usuario" ],
         "expires": "1d",
+        "canUpdate": [ "usuario", "gestor" ],
+        "canDelete": [ "usuario" ],
     }
 }
+
+module.exports = permissions
